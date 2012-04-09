cd8892e02cc0cea7ee3654776db82bcb6f66af75
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-04-09 02:42:11 +0000

    Small change in ast
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@84 e6381bd7-9cb6-4842-97e7-9f92de067d63

--- src/ast.py
+++ src/ast.py
@@ -37,13 +37,6 @@ class AndExpr(Expr):
 class And(AndExpr):
     def __init__(self, left, right):
         AST.__init__(self, (left,right))
-    def typecheck(self):
-        if (left.typecheck() == bool and right.typecheck() == bool):
-            self.nodeType = bool
-            return bool
-        else:
-            # error
-            pass
 
 class EqualExpr(AndExpr):
     pass
