6fe58d07b486b2d2dd49623af12ee11ad0c4ba73
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-04-09 17:55:56 +0000

    Generate lexer test cases from lexout
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@87 e6381bd7-9cb6-4842-97e7-9f92de067d63

--- src/tests.py
+++ src/tests.py
@@ -37,7 +37,7 @@ class LexerTest(FileTest):
 
     @classmethod
     def __static__(cls):
-        for name, p, expected in testFiles('out'):
+        for name, p, expected in testFiles('lexout'):
             def makeTest(p, expected):
                 def runTest(self):
                     scanner = lexer.MiniJavaScanner()
