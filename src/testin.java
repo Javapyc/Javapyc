fb2ea60255ac17f978954e9e6c5f2cc14fed2b45
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-04-11 16:51:32 +0000

    tweak testin.java
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@97 e6381bd7-9cb6-4842-97e7-9f92de067d63

--- src/testin.java
+++ src/testin.java
@@ -1,44 +1,52 @@
-System.out.println(1+2);
-/*System.out.println( (3+4)*4 + (2-1)/1/1/1/1/1*1*1*2/2 - ((4*4-1)+1)/16);*/
-System.out.println(10/5);
-System.out.println(Math.pow(2, 4*2) - 1);
-System.out.println(1+2-1);
-System.out.println(1--1+2+-2);
-System.out.println(false && false || false && true || true && true);
-System.out.println(!(1+2 < -1));
-System.out.println(1 == 2);
-System.out.println(1 <= 2 != false && true || false);
+//public class Program {
+//	public static void main(String[] args) {
+		System.out.println(1+2);
+		/*System.out.println( (3+4)*4 + (2-1)/1/1/1/1/1*1*1*2/2 - ((4*4-1)+1)/16);*/
+		System.out.println(10/5);
+		System.out.println(Math.pow(2, 4*2) - 1);
+		System.out.println(1+2-1);
+		System.out.println(1--1+2+-2);
+		System.out.println(false && false || false && true || true && true);
+		System.out.println(!(1+2 < -1));
+		System.out.println(1 == 2);
+		System.out.println(1 <= 2 != false && true || false);
 
-int a = 5+1;
-System.out.println((3+4)*a);
-a = a + 4;
-System.out.println(a);
+		int a = 5+1;
+		System.out.println((3+4)*a);
+		a = a + 4;
+		System.out.println(a);
 
-//if (10 < 17) System.out.println(5); else System.out.println(4);
-if (true) {int a = 4;} else int r = 3; int t = 34;
-if (false || true) {
-    System.out.println(5 + 6);
-    System.out.println(1 -33);
-}else{
-    System.out.println(-555);
+		//if (10 < 17) System.out.println(5); else System.out.println(4);
+		if (true) {int a = 4;} else int r = 3; int t = 34;
+		if (false || true) {
+		    System.out.println(5 + 6);
+		    System.out.println(1 -33);
+		} else {
+		    System.out.println(-555);
+		}
+
+		boolean check = true;
+		int count = 0;
+		while (check){
+			if (count > 5) {check = false;}else{}
+			System.out.println(count);
+			count = count + 1;
+		}
+		System.out.println(count);
+
+		if (true) {
+		    while (false) int a = 3;
+		    System.out.println(5);
+		}else{}
+//	}
+//}
+
+/*
+public class TestClass {
+	public int kevin(boolean r) {
+
+	    System.out.println(4); 
+	    return 5; 
+	}
 }
-boolean check = true;
-int count = 0;
-while (check){
-    if (count > 5) {check = false;}else{}
-    System.out.println(count);
-    count = count + 1;
-    }
-System.out.println(count);
-
-if (true) {
-    while (false) int a = 3;
-    System.out.println(5);
-}else{}
-
-public int kevin(boolean r){ 
-    System.out.println(4); 
-    return 5; 
-}
-
-
+*/
