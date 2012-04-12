d7ed3bd42cdbadf2db4e5fd961e1bf86a0b65a03
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-04-12 01:08:41 +0000

    Add parser test case
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@104 e6381bd7-9cb6-4842-97e7-9f92de067d63

---
+++ src/tests/parser.java
@@ -0,0 +1,16 @@
+
+class Program {
+	public static void main(String[] args) {
+
+		int n = new Null().a().b(1).c(true, false, (this), null).d(new Null().e());
+
+		n = -x.y() * a.b() + (c.d()).e().f().g();
+	}
+}
+
+class Null {
+}
+
+class Field {
+	int a;
+}
