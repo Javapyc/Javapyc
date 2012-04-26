b0b79f5d8615edc117e446fb62f322ec2f53c3cc
mayhewsw <mayhewsw@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-04-26 22:13:41 +0000

    Branchtest.java for testing branches
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@129 e6381bd7-9cb6-4842-97e7-9f92de067d63

---
+++ src/branchtest.java
@@ -0,0 +1,13 @@
+class Joe{
+    public static void main(String[] args){
+        int a = 4;
+        int b = 33;
+
+        while(a < b){
+            System.out.printf("A is still less than B\n");
+            a = a + 5;
+        }
+        System.out.printf("Finally! A is greater than B!\n");
+        
+    }
+}
