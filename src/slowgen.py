13282fa6e0544e7078788ef067d396de4586e7a5
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-05-07 17:17:32 +0000

    Add very basic main slowgen
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@149 e6381bd7-9cb6-4842-97e7-9f92de067d63

--- src/slowgen.py
+++ src/slowgen.py
@@ -7,6 +7,45 @@ from codegen import codegens
 from code import CodeGen, CmpOp, Flags
 import ast
 
+@codegens(ast.Program)
+def codegen(self, c):
+    mainclass, *classes = self.children
+    
+    c.setLine(1)
+    
+    #make the only function
+    main = CodeGen(c.filename, 'program')
+    main.context = mainclass.context
+    main.setFlags(Flags.NEWLOCALS | Flags.OPTIMIZED)
+    #TODO other classes
+    mainclass.codegen(main)
+    c.LOAD_CONST(main)
+    c.MAKE_FUNCTION()
+    c.STORE_NAME('main')
+
+    #ifmain
+    c.LOAD_NAME('__name__')
+    c.LOAD_CONST('__main__')
+    c.COMPARE_OP(CmpOp.EQUAL)
+    dest = c.POP_JUMP_IF_FALSE()
+    c.LOAD_NAME('main')
+    c.CALL_FUNCTION()
+    c.POP_TOP()
+
+    #module return
+    dest()
+    c.LOAD_CONST(None)
+    c.RETURN_VALUE()
+
+@codegens(ast.MainClassDecl)
+def codegen(self, c):
+    stmts = self.children
+    for stmt in stmts:
+        stmt.codegen(c)
+    
+    c.LOAD_CONST(None)
+    c.RETURN_VALUE()
+
 @codegens(ast.If)
 def codegen(self, c):
     cond, ifstmt, elsestmt = self.children
@@ -52,5 +91,5 @@ def codegen(self, c):
     # Mark the end of the while loop
     jumpLoc()
 
-raise Exception('slowgen codegen not implemented')
+print('WARNING: slowgen codegen not fully implemented')
 
