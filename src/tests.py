1a628f58e3843b4ddbedd87e5beddea3f915b3ff
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-05-11 15:24:59 +0000

    Add codegen test cases
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@163 e6381bd7-9cb6-4842-97e7-9f92de067d63

--- src/tests.py
+++ src/tests.py
@@ -5,12 +5,15 @@ import os.path
 import sys
 import re
 import unittest
-import lexer
-import parser
 import subprocess
 from glob import iglob as glob
 from util import staticinit
+
 import spark
+import lexer
+import parser
+import typechecker
+import codegen
 
 from fileutils import TempFile
 
@@ -91,6 +94,39 @@ class ParserTests(FileTest):
     def test_errors(self):
         p = 'tests/simple_parseerror.java'
         self.checkError(p)
+
+@staticinit
+class ParserTests(FileTest):
+
+    @classmethod
+    def setUpClass(cls):
+        subprocess.call(['rm', '-rf', 'testbins'])
+        subprocess.call(['mkdir', 'testbins'])
+
+    @classmethod
+    def __static__(cls):
+        for name, p, expected in testFiles('out'):
+            def makeTest(name, p, expected):
+                def runTest(self):
+                    scanner = lexer.MiniJavaScanner()
+                    with open(p) as f: s = f.read()
+                    tokens = scanner.tokenize(s)
+                    javaParser = parser.ProgramParser()
+                    tree = javaParser.parse(tokens)
+                    typechecker.typecheck(tree)
+                    binpath = os.path.join('testbins', name + '.pyc')
+                    codegen.codegen(binpath, tree, False)
+                    with TempFile() as fout:
+                        #run program
+                        proc = subprocess.Popen(('python3', binpath),
+                                                stdout=fout.f,
+                                                stderr=subprocess.STDOUT)
+                        res = proc.wait()
+                        self.assertEqual(0, res)
+                        fout.flush()
+                        self.diff(expected, fout.name)
+                return runTest
+            setattr(cls, name, makeTest(name, p, expected))
 
 if __name__ == '__main__':
     unittest.main()
