b31ab5e01325faba688427056600ed450d14b0f3
wellska1 <wellska1@e6381bd7-9cb6-4842-97e7-9f92de067d63>
2012-05-13 00:46:30 +0000

    Retab.  Change If to IfElse
    git-svn-id: http://svn.csse.rose-hulman.edu/repos/csse404-201230-javapyc/trunk@183 e6381bd7-9cb6-4842-97e7-9f92de067d63

--- src/optimizer.py
+++ src/optimizer.py
@@ -24,24 +24,24 @@ def optimizes(cls):
 
 
 def collapseConstantInt(self):
-	left, right = self.children
+    left, right = self.children
 
-	leftVal = None
-	if isinstance(left, ast.ID):
-		leftVal = left.context.getConstVar(left.name)
-	elif isinstance(left, ast.Integer):
-		leftVal = left.val
+    leftVal = None
+    if isinstance(left, ast.ID):
+        leftVal = left.context.getConstVar(left.name)
+    elif isinstance(left, ast.Integer):
+        leftVal = left.val
 
 
-	rightVal = None
-	if isinstance(right, ast.ID):
-		rightVal = right.context.getConstVar(right.name)
-	elif isinstance(right, ast.Integer):
-		rightVal = right.val
+    rightVal = None
+    if isinstance(right, ast.ID):
+        rightVal = right.context.getConstVar(right.name)
+    elif isinstance(right, ast.Integer):
+        rightVal = right.val
 
-	if isinstance(leftVal, int) and isinstance(rightVal, int):
-		return ast.Integer(leftVal + rightVal)
-	return self
+    if isinstance(leftVal, int) and isinstance(rightVal, int):
+        return ast.Integer(leftVal + rightVal)
+    return self
 
 def collapseBool(self, combine):
     left, right = self.children
@@ -63,13 +63,13 @@ def collapseInt(self, combine):
 
 @optimizes(ast.ID)
 def optimize(self):
-	if isinstance(self.context.varType(self.name), ast.BasicType):
-		# FIXME TODO this feels wrong with "== int"
-		if self.context.varType(self.name).basicType == int:
-			test = self.context.getConstVar(self.name)
-			if test:
-				return ast.Integer(test)
-	return self
+    if isinstance(self.context.varType(self.name), ast.BasicType):
+        # FIXME TODO this feels wrong with "== int"
+        if self.context.varType(self.name).basicType == int:
+            test = self.context.getConstVar(self.name)
+            if test:
+                return ast.Integer(test)
+    return self
 
 @optimizes(ast.Or)
 def optimize(self):
@@ -100,7 +100,7 @@ def optimize(self):
 @optimizes(ast.LessThan)
 def optimize(self):
     return collapseComp(self, lambda a,b: a < b)
-	
+    
 
 @optimizes(ast.GreaterThan)
 def optimize(self):
@@ -118,7 +118,7 @@ def optimize(self):
 def optimize(self):
     #TODO bound check
 #    return collapseInt(self, lambda a,b: a+b)
-	return collapseConstantInt(self)
+    return collapseConstantInt(self)
 
 @optimizes(ast.Minus)
 def optimize(self):
@@ -154,70 +154,71 @@ def optimize(self):
     return self
 
 
-@optimizes(ast.If)
+@optimizes(ast.IfElse)
 def optimize(self):
-	(cond, ifstmt, elsestmt) = self.children
-	if isinstance(cond, ast.Boolean):
-		if cond.val:
-			return ifstmt
-		else:
-			return elsestmt
-	return self
+    (cond, ifstmt, elsestmt) = self.children
+    if isinstance(cond, ast.Boolean):
+        if cond.val:
+            return ifstmt
+        else:
+            return elsestmt
+    return self
 
 @optimizes(ast.While)
 def optimize(self):
-	(cond, stmt) = self.children
-	if isinstance(cond, ast.Boolean) and not cond.val:
-		return ast.StmtList(tuple())
-	return self
+    (cond, stmt) = self.children
+    if isinstance(cond, ast.Boolean) and not cond.val:
+        return ast.StmtList(tuple())
+    return self
 
 
 def optimizeStmtList(stmts):
-	# Constant propogation (search for ID which are constant and pass constant instead of ID)
+    # Constant propogation (search for ID which are constant and pass constant instead of ID)
 
-	newstmts = []
-	for stmt in stmts:
-		stmt = stmt.optimize()
+    newstmts = []
+    for stmt in stmts:
+        stmt = stmt.optimize()
 
-		if isinstance(stmt, ast.Decl) and isinstance(stmt.typename, ast.BasicType) and isinstance(stmt.children[0], ast.Integer):
-			# have a named variable with a constant value (a = 10, b = 15)
-			stmt.context.declareConstVar(stmt.name, stmt.children[0].val)
-			print ("\tFound", stmt.name, "=>", stmt.children[0].val)
+        if isinstance(stmt, ast.Decl) and isinstance(stmt.typename, ast.BasicType) and isinstance(stmt.children[0], ast.Integer):
+            # have a named variable with a constant value (a = 10, b = 15)
+            stmt.context.declareConstVar(stmt.name, stmt.children[0].val)
+            print("\tFound", stmt.name, "=>", stmt.children[0].val)
 
-		if isinstance(stmt, ast.Assignment):
-			if isinstance(stmt.children[0], ast.Integer):
-				stmt.context.declareConstVar(stmt.name, stmt.children[0].val)
-				print ("\tMapped", stmt.name, "=>", stmt.children[0].val)
-			else:
-				print ("\t", stmt.name, "MAY no longer const")
-				stmt.context.declareConstVar(stmt.name, None)
+        if isinstance(stmt, ast.Assignment):
+            if isinstance(stmt.children[0], ast.Integer):
+                stmt.context.declareConstVar(stmt.name, stmt.children[0].val)
+                print("\tMapped", stmt.name, "=>", stmt.children[0].val)
+            else:
+                print("\t", stmt.name, "MAY no longer const")
+                stmt.context.declareConstVar(stmt.name, None)
 
 
-		newstmts.append(stmt)
-	return newstmts
+        newstmts.append(stmt)
+    return newstmts
 
 
 #@optimizes(ast.ClassDecl)
 #def optimize(self):
-	#TODO add similiar code from ast.StmtList so that optimizer will use const in MethodDecl	
-#	pass
+    #TODO add similiar code from ast.StmtList so that optimizer will use const in MethodDecl    
+#   pass
 
 #@optimizes(ast.MethodDecl
 #def optimize(self):
-	#TODO add similiar code from ast.StmtList so that optimizer will use const in MethodDecl	
-#	pass
+    #TODO add similiar code from ast.StmtList so that optimizer will use const in MethodDecl    
+#   pass
 
 #@optimizes(ast.StmtList)
 #def optimize(self):
-	#TODO add similiar code from ast.StmtList so that optimizer will use const in MethodDecl	
-#	pass
+    #TODO add similiar code from ast.StmtList so that optimizer will use const in MethodDecl    
+#   pass
 
 
 @optimizes(ast.MainClassDecl)
 def optimize(self):
-	# TODO call while change is happening
-	self.children = optimizeStmtList(self.children)
-	self.children = tuple(map(lambda child: child.optimize(), self.children))
-	return self
+    # TODO call while change is happening
+    print('optimizing')
+    self.children = optimizeStmtList(self.children)
+    self.children = tuple(map(lambda child: child.optimize(), self.children))
+    return self
 
 
